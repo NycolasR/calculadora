7aae8b20f39509f0ca079cf39e4d224fc22e30a9
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-11-03 01:16:15 -0300

    Update calculadora.service.ts

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -1,9 +1,55 @@
+/**
+ * Serviço responsável por executar as operações da
+ * calculadora.
+ *
+ * @author Nycolas R. Alves<nycolasramon3@gmail.com>
+ * @since 1.0.0
+ */
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CalculadoraService {
+  /* Define as constantes utilizadas
+      para identificar as operações de cálculo */
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly MULTIPLICACAO: string = '*';
+  static readonly DIVISAO: string = '/';
 
-  constructor() { }
+  constructor() {}
+
+  /**
+   * Método que realiza uma operação matemática dados
+   * dois números.
+   * Suporta as operações de soma, subtração, multiplicação
+   * e divisão.
+   *
+   * @param num1 number
+   * @param num2 number
+   * @param operacao string Operação a ser executada
+   * @returns number Resultado da operação
+   */
+  calcular(num1: number, num2: number, operacao: string): number {
+    let resultado: number = 0; // armazena o resultado da operação
+
+    switch (operacao) {
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+        break;
+    }
+
+    return resultado;
+  }
 }
